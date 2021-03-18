20fd4770014260401928c4ee9982d81e9970201c
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2021-03-18 14:03:53 +0100

    Modificacion de codigo y JavaDoc

--- src/gestenis/Tenista.java
+++ src/gestenis/Tenista.java
@@ -3,155 +3,175 @@ package gestenis;
 import java.io.*;
 import java.util.ArrayList;
 
-
+/**
+ * Esta clase nos permite obtener y asociar datos acerca de los tenistas
+ * registrados en una competicion.
+ * 
+ * @author Antonio Sanchez
+ *
+ */
 public class Tenista {
-    private String nombre;
-    private int edad;
-    private ArrayList<Torneo> palmares;
-    
-    Tenista(String nombre,int edad){
-        this.nombre=nombre;
-        this.edad=edad;
-        //lista de torneos
-        palmares=new ArrayList<Torneo>();
-    }
+	private String nombre;
+	private int edad;
+	private ArrayList<Torneo> palmares;
 
-    /**
-     * Obtenemos el nombre del tenista
-     * @return nombre
-     */
-    public String getNombre() {
-        return nombre;
-    }
+	/**
+	 * Constructor para introducir un nuevo tenista.
+	 * 
+	 * @param nombre del tenista
+	 * @param edad   del tenista
+	 */
+	Tenista(String nombre, int edad) {
+		this.nombre = nombre;
+		this.edad = edad;
+		// lista de torneos
+		palmares = new ArrayList<Torneo>();
+	}
 
-    /**
-     * Asignamos el nombre del tenista
-     * @param nombre
-     */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	/**
+	 * Obtenemos el nombre del tenista
+	 * 
+	 * @return nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
 
-    /**
-     * Nos devuelve la edad del tenista
-     * @return la edad
-     */
-    public int getEdad() {
-        return edad;
-    }
+	/**
+	 * Asignamos el nombre del tenista
+	 * 
+	 * @param nombre
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    /**
-     * Asigna la edad del tenista
-     * @param edad
-     */
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-    /*
-     * Devuelve el palmarés con los nombres de los torneos ganados
-     * @return
-     */
-    public String[] getPalmares(){
-        String[] p = new String[palmares.size()];
-        for(int i=0;i<p.length;i++){
-            p[i] = palmares.get(i).getNombreTor();
-        }
-        return p;
-    }
-    /**
-     * Añade el torneo ganado al palmarés
-     * @param torneo
-     */
-    public void aniadirPalmares(Torneo torneo){
-        palmares.add(torneo);
-    }
-    
-    /*
-     * Recorre un bucle acumulando la puntuación obtenida
-     * 
-     */
-    public int getPuntuacionATP(){
-    	int p = 1;
-        p = 0;
-        for(Torneo t:palmares){
-            p += t.getPunt();
-        }
-        return p;
-    }
-    /**
-     * Carga en fichero el archivo seleccionado e introduce
-     * sus datos en el ArrayList Tenista y devuelve lista
-     * @param fichero
-     * @return
-     */
-	public static ArrayList<Tenista> cargar(File fichero){
-        ArrayList<Tenista> l = null;
-        ObjectInputStream ficheroEntrada = null;
-        try{
-            ficheroEntrada = new ObjectInputStream(new FileInputStream(fichero));
-            l = (ArrayList<Tenista>) ficheroEntrada.readObject();
-            ficheroEntrada.close();
-            return l;
-        }catch(ClassNotFoundException onfe){
-            return null;
-        }catch(FileNotFoundException fnfe){
-            return null;
-        }catch(IOException ioe){
-            return null;
-        }
-    }
-    /**
-     * Graba en fichero los datos del ArrayList lista
-     * devolviendo true si ha sido todo correcto o false en caso contrario
-     * @param lista
-     * @param fichero
-     * @return
-     */
-    public static boolean guardar(ArrayList<Tenista> lista, File fichero){
-        try{
-            ObjectOutputStream ficheroSalida = new ObjectOutputStream(new FileOutputStream(fichero));
-            ficheroSalida.writeObject(lista);
-            ficheroSalida.flush();
-            ficheroSalida.close();
-            return true;
-        }catch(FileNotFoundException fnfe){
-            return false;
-        }catch(IOException ioe){
-            return false;
-        }
-    }
-    
-    public static boolean cargarGuardar(ArrayList<Tenista> lista, File fichero){
-    	ArrayList<Tenista> l = null;
-        ObjectInputStream ficheroEntrada = null;
-        try{
-            ficheroEntrada = new ObjectInputStream(new FileInputStream(fichero));
-            l = (ArrayList<Tenista>) ficheroEntrada.readObject();
-           
-            cierraFichero(ficheroEntrada);
+	/**
+	 * Nos devuelve la edad del tenista
+	 * 
+	 * @return la edad
+	 */
+	public int getEdad() {
+		return edad;
+	}
 
-        }catch(ClassNotFoundException onfe){
-            return false;
-        }catch(FileNotFoundException fnfe){
-            return false;
-        }catch(IOException ioe){
-            return false;
-        }
-        try{
-            ObjectOutputStream ficheroSalida = new ObjectOutputStream(new FileOutputStream(fichero));
-            ficheroSalida.writeObject(lista);
-            ficheroSalida.flush();
-            ficheroSalida.close();
-            return true;
-        }catch(FileNotFoundException fnfe){
-            return false;
-        }catch(IOException ioe){
-            return false;
-        }
-        
-    }
-    
-    public static void cierraFichero(ObjectInputStream ficheroEntrada) throws IOException {
-    	ficheroEntrada.close();
-    }
+	/**
+	 * Asigna la edad del tenista
+	 * 
+	 * @param edad
+	 */
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	/*
+	 * Devuelve el palmares con los nombres de los torneos ganados
+	 * 
+	 * @return
+	 */
+	public String[] getPalmares() {
+		String[] p = new String[palmares.size()];
+		for (int i = 0; i < p.length; i++) {
+			p[i] = palmares.get(i).getNombreTorneo();
+		}
+		return p;
+	}
+
+	/**
+	 * Añade el torneo ganado al palmares
+	 * 
+	 * @param torneo objeto torneo
+	 */
+	public void aniadirPalmares(Torneo torneo) {
+		palmares.add(torneo);
+	}
+
+	/**
+	 * Recorre un bucle acumulando la puntuación obtenida.
+	 * 
+	 * @return p - puntacion obtenida de los torneos
+	 */
+	public int getPuntuacionATP() {
+		int p = 0;
+		for (Torneo t : palmares) {
+			p += t.getPuntuacion();
+		}
+		return p;
+	}
+
+	/**
+	 * Carga el archivo seleccionado e introduce sus datos en el ArrayList Tenista y
+	 * devuelve la lista con los tenista.
+	 * 
+	 * @param fichero - fichero que cargamos
+	 * @return l - lista de los tenistas
+	 */
+	public static ArrayList<Tenista> cargar(File fichero) {
+		ArrayList<Tenista> l = null;
+		ObjectInputStream ficheroEntrada = null;
+		try {
+			ficheroEntrada = new ObjectInputStream(new FileInputStream(fichero));
+			l = (ArrayList<Tenista>) ficheroEntrada.readObject();
+			ficheroEntrada.close();
+		} catch (ClassNotFoundException onfe) {
+			l = null;
+		} catch (FileNotFoundException fnfe) {
+			l = null;
+		} catch (IOException ioe) {
+			l = null;
+		}
+		return l;
+	}
+
+	/**
+	 * Guarda en un fichero los datos de la lista de los tenistas devolviendo true
+	 * si ha sido todo correcto o false en caso contrario.
+	 * 
+	 * @param lista   de los tenistas
+	 * @param fichero donde vamos a guardar la lista
+	 * @return validador - true o false
+	 */
+	public static boolean guardar(ArrayList<Tenista> lista, File fichero) {
+		boolean validador = false;
+		try {
+			ObjectOutputStream ficheroSalida = new ObjectOutputStream(new FileOutputStream(fichero));
+			ficheroSalida.writeObject(lista);
+			ficheroSalida.flush();
+			ficheroSalida.close();
+			validador = true;
+		} catch (FileNotFoundException fnfe) {
+			validador = false;
+		} catch (IOException ioe) {
+			validador = false;
+		}
+		return validador;
+	}
+
+	/**
+	 * Cargar un fichero con los datos los tenistas devolviendo true si ha sido todo
+	 * correcto o false en caso contrario.
+	 * 
+	 * @param lista   de los tenistas
+	 * @param fichero de donde vamos a cargar la lista
+	 * @return validador - true o false
+	 */
+	public static boolean cargar(ArrayList<Tenista> lista, File fichero) {
+		boolean validador = false;
+		ArrayList<Tenista> l = null;
+		ObjectInputStream ficheroEntrada = null;
+		try {
+			ficheroEntrada = new ObjectInputStream(new FileInputStream(fichero));
+			l = (ArrayList<Tenista>) ficheroEntrada.readObject();
+			validador = true;
+			ficheroEntrada.close();
+
+		} catch (ClassNotFoundException onfe) {
+			validador = false;
+		} catch (FileNotFoundException fnfe) {
+			validador = false;
+		} catch (IOException ioe) {
+			validador = false;
+		}
+		return validador;
+	}
 }
--- src/gestenis/Torneo.java
+++ src/gestenis/Torneo.java
@@ -3,134 +3,142 @@ package gestenis;
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ * Esta clase nos permite obtener y asociar datos acerca de los torneos.
+ * 
+ * @author Antonio Sanchez
+ *
+ */
+public class Torneo {
 
-public class Torneo  {
-	
-	//No usado:
-    public static String GRAND_SLAM = "Grand Slam";
-    public static String MASTER1000 = "ATP World Tour Master 1000";
-    public static String MASTER500 = "ATP World Tour 500";
-    public static String MASTER250 = "ATP World Tour 250";
-    
-    private String nombreTor;
-    private int punt;
-    
-    Torneo (String nombreTor,int punt){
-        this.nombreTor=nombreTor;
-        this.punt=punt;
-    }
+	private String nombreTorneo;
+	private int puntuacion;
 
-    /**
-     * Devuelve el nombre del torneo
-     * @return
-     */
-    public String getNombreTor() {
-        return nombreTor;
-    }
+	/**
+	 * Constructor para introducir un nuevo torneo.
+	 * 
+	 * @param nombreTorneo - nombre del torneo
+	 * @param puntuacion   del torneo
+	 */
+	Torneo(String nombreTorneo, int puntuacion) {
+		this.nombreTorneo = nombreTorneo;
+		this.puntuacion = puntuacion;
+	}
 
-    /**
-     * Asignamos un nombre de torneo
-     * @param nombreTor del torneo
-     */
-    public void setNombreTor(String nombreTor) {
-        this.nombreTor = nombreTor;
-    }
+	/**
+	 * Devuelve el nombre del torneo
+	 * 
+	 * @return nombreTorneo - nombre del torneo
+	 */
+	public String getNombreTorneo() {
+		return nombreTorneo;
+	}
 
-    /**
-     * Devuelve la puntuación asignada al torneo
-     * @return
-     */
-    public int getPunt() {
-        return punt;
-    }
+	/**
+	 * Asignamos un nombre de torneo
+	 * 
+	 * @param nombreTorneo - nombre del torneo
+	 */
+	public void setNombreTorneo(String nombreTorneo) {
+		this.nombreTorneo = nombreTorneo;
+	}
 
-    /**
-     * Introducimos la puntuación asignada para el torneo
-     * @param
-     */
-    public void setPunt(int punt) {
-        this.punt = punt;
-    }
-    /**
-     * Carga los datos del fichero en el ArrayList lista
-     * y devuelve true si todo ha ido bien o false si ha fallado algo
-     * @param fichero
-     * @return
-     */
-    public static ArrayList<Torneo> cargar(File fichero){
-        ArrayList<Torneo> lista = null;
-        try{
-        	lista = new ArrayList<Torneo>();
-            ObjectInputStream ficheroEntrada = null;
-            ficheroEntrada = new ObjectInputStream(new FileInputStream(fichero));
-            lista = (ArrayList<Torneo>) ficheroEntrada.readObject();
-            ficheroEntrada.close();
-            return lista;
-        }catch(ClassNotFoundException cnfe){
-            return null;
-        }catch(FileNotFoundException fnfe){
-            return null;
-        }catch (IOException ioe){
-            return null;
-        }
-    }
-    /**
-     * Guarda los datos del ArrayList lista en el fichero fichero
-     * Si todo ha ido bien devuelve true y en caso contrario false 
-     * @param lista
-     * @param fichero
-     * @return
-     */
-    public static boolean guardar(ArrayList<Torneo> lista,File fichero){
-        try{
-        	//Fichero de salida
-            ObjectOutputStream sal = null; 
-            sal = new ObjectOutputStream(new FileOutputStream (fichero));
-            sal.writeObject(lista);
-            sal.flush();
-            sal.close();
-            return true;
-        }catch(FileNotFoundException fnfe){
-            return false;
-        }catch(IOException ioe){
-            return false;
-        }
-    }
-    
-    /**
-     * Carga los datos del fichero en el ArrayList lista, luego guarda los datos del ArrayList lista en el fichero fichero
-     * Si todo ha ido bien devuelve true y en caso contrario false.
-     * @param lista
-     * @param fichero
-     * @return
-     */
-    public static boolean cargarYGuardar(ArrayList<Torneo> lista, File fichero){
-        
-    	//cargar
-        try{
-            ObjectInputStream ficheroEntrada = new ObjectInputStream(new FileInputStream(fichero));
-            lista = (ArrayList<Torneo>) ficheroEntrada.readObject();
-            ficheroEntrada.close();
+	/**
+	 * Devuelve la puntuación asignada al torneo
+	 * 
+	 * @return puntuacion del torneo
+	 */
+	public int getPuntuacion() {
+		return puntuacion;
+	}
 
-        }catch(ClassNotFoundException cnfe){
-            return false;
-        }catch(FileNotFoundException fnfe){
-            return false;
-        }catch (IOException ioe){
-            return false;
-        }
-        
-        //guardar
-    	try{
-            ObjectOutputStream ficheroSalida = new ObjectOutputStream(new FileOutputStream (fichero));
-            ficheroSalida.writeObject(lista);
-            ficheroSalida.flush();
-            ficheroSalida.close();
-            return true;
-        }catch(FileNotFoundException fnfe){
-            return false;
-        }catch(IOException ioe){
-            return false;
-        }
-    }
+	/**
+	 * Introducimos la puntuación asignada para el torneo
+	 * 
+	 * @param puntuacion del torneo
+	 */
+	public void setPuntuacion(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+
+	/**
+	 * Carga los datos del fichero en el ArrayList lista y devuelve true si todo ha
+	 * ido bien o false si ha fallado algo
+	 * 
+	 * @param fichero que cargamos
+	 * @return validador - true o false
+	 */
+	public static boolean cargar(File fichero) {
+		boolean validador = false;
+		ArrayList<Torneo> lista = null;
+		try {
+			lista = new ArrayList<Torneo>();
+			ObjectInputStream ficheroEntrada = null;
+			ficheroEntrada = new ObjectInputStream(new FileInputStream(fichero));
+			lista = (ArrayList<Torneo>) ficheroEntrada.readObject();
+			ficheroEntrada.close();
+			validador = true;
+		} catch (ClassNotFoundException cnfe) {
+			validador = false;
+		} catch (FileNotFoundException fnfe) {
+			validador = false;
+		} catch (IOException ioe) {
+			validador = false;
+		}
+		return validador;
+	}
+
+	/**
+	 * Guarda los datos del ArrayList lista en el fichero fichero Si todo ha ido
+	 * bien devuelve true y en caso contrario false
+	 * 
+	 * @param lista   de torneos
+	 * @param fichero que queremos guardar
+	 * @return validador - true o false
+	 */
+	public static boolean guardar(ArrayList<Torneo> lista, File fichero) {
+		boolean validador = false;
+		try {
+			// Fichero de salida
+			ObjectOutputStream ficheroSalida = null;
+			ficheroSalida = new ObjectOutputStream(new FileOutputStream(fichero));
+			ficheroSalida.writeObject(lista);
+			ficheroSalida.flush();
+			ficheroSalida.close();
+			validador = true;
+		} catch (FileNotFoundException fnfe) {
+			validador = false;
+		} catch (IOException ioe) {
+			validador = false;
+		}
+
+		return validador;
+	}
+
+	/**
+	 * Carga los datos del fichero en el ArrayList lista, luego guarda los datos del
+	 * ArrayList lista en el fichero Si todo ha ido bien devuelve true y en caso
+	 * contrario false.
+	 * 
+	 * @param lista   de torneo
+	 * @param fichero que queremos cargar
+	 * @return validador - true o false
+	 */
+	public static boolean cargar(ArrayList<Torneo> lista, File fichero) {
+		boolean validador = false;
+		// Cargar fichero.
+		try {
+			ObjectInputStream ficheroEntrada = new ObjectInputStream(new FileInputStream(fichero));
+			lista = (ArrayList<Torneo>) ficheroEntrada.readObject();
+			ficheroEntrada.close();
+			validador = true;
+		} catch (ClassNotFoundException cnfe) {
+			validador = false;
+		} catch (FileNotFoundException fnfe) {
+			validador = false;
+		} catch (IOException ioe) {
+			validador = false;
+		}
+		return validador;
+	}
 }
